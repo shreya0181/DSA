27083b4930ca6af0fecd3c1d5ae171b4f693da3e
shreya0181 <shreya171769.cse@chitkara.edu.in>
2021-01-22 12:02:50 +0530

    implementing Stack Using Array

--- IDS/Stacks/StacksUsingArray.java
+++ IDS/Stacks/StacksUsingArray.java
@@ -4,12 +4,12 @@ public class StacksUsingArray {
     // LIFO
     int array[] ;
     int size;
-    int start=0;
-    int end;
-    StacksUsingArray(int size)
+    int top=-1;
+   
+    StacksUsingArray()
     {
-        this.size= size;
-        array = new int[size];
+       
+        array = new int[10];
     }
 
     public boolean isEmpty()
@@ -21,25 +21,13 @@ public class StacksUsingArray {
     {
     
        if(isEmpty()) throw new NullPointerException();
-       int poll=-1;
-       if(!isEmpty())
-       {
-        poll= array[start];
-        start--;
-       }
-       return poll;
+       return array[top--];
     }
 
     public void push(int element)
     {
-        for(int i=start;i<end;i++)
-        {
-            array [i+1] = array[i];
-        }
-        array[start]= element;
-        end++;
-        size++;
-
+       if(top>9) System.out.print("Stack Over Flow");
+       array[++top]= element;
     }
 
 }
